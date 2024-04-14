6f6b05ad875308e0eee2c0393cd723e3c16c528f
Taro Morita <morita0079@gmail.com>
2024-04-14 14:28:40 +0900

    Refactor DatabaseHandler.py to remove commented out code and unused imports

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,16 +1,5 @@
 import mysql.connector as mydb
 
-# conn = mydb.connect(
-#     host="localhost",
-#     user="app_user",
-#     port="3306",
-#     password="!ChangeMe!",
-#     database="app_db",
-# )
-
-# conn.ping(reconnect=True)
-# print(conn.is_connected())
-
 
 class DataBaseHandler:
     def __init__(self, host: str, user: str, port: str, password: str, database: str):
