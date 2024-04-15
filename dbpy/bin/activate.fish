32fc32df01bce9ddda59fa99c6706123e8cef591
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-04-16 01:13:40 +0900

    add configuration

--- dbpy/bin/activate.fish
+++ dbpy/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/taro_morita/dev/oms-dbpy/dbpy"
+set -gx VIRTUAL_ENV "/Users/awesometaro/dev/oms-dbpy/dbpy"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
